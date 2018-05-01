a316d86013170f76607174476f0f04315e4f7c1d
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-01 02:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much.";
 
 function destructivelyAppendKitten(name) {
   var kittens = ['Milo', 'Otis', 'Garfield'];
-  kittens.unshift(name);
+  kittens.push(name);
     return kittens;
 }
